e998905556e1aa8a4a22f880f6c9dbbce09a8acc
lipinski pawel <plipinski@users.noreply.github.com>
2011-05-25 11:32:12 +0200

    classes without toString handled newline and parantheses in params handled fix of bug when single paramset used

--- src/main/java/junitparams/Utils.java
+++ src/main/java/junitparams/Utils.java
@@ -7,23 +7,55 @@ package junitparams;
  */
 class Utils {
 
-    public static String stringify(Object paramSet) {
-        if (paramSet instanceof String)
-            return (String) paramSet;
-        Object[] params = (Object[]) paramSet;
+    public static String stringify(Object paramSet, int paramIdx) {
+        String result = "[" + paramIdx + "] ";
 
-        return asCsvString(params);
+        if (paramSet instanceof String)
+            result += paramSet;
+        else
+            result += asCsvString(safelyCastParamsToArray(paramSet), paramIdx);
+
+        return trimSpecialChars(result);
     }
 
-    private static String asCsvString(Object[] params) {
+    private static String trimSpecialChars(String result) {
+        return result.replace('\n', ' ').replace('(', '[').replace(')', ']');
+    }
+
+    static Object[] safelyCastParamsToArray(Object paramSet) {
+        Object[] params;
+        try {
+            params = (Object[]) paramSet;
+        } catch (ClassCastException e) {
+            params = new Object[] { paramSet };
+        }
+        return params;
+    }
+
+    private static String asCsvString(Object[] params, int paramIdx) {
         String result = "";
 
         for (Object param : params) {
-            result += param.toString();
+            result = addParamToResult(result, param);
+
             if (param != params[params.length - 1])
                 result += ", ";
         }
 
+        return result;
+    }
+
+    private static String addParamToResult(String result, Object param) {
+        if (param == null)
+            result += "null";
+        else {
+            try {
+                param.getClass().getDeclaredMethod("toString");
+                result += param.toString();
+            } catch (Exception e) {
+                result += param.getClass().getSimpleName();
+            }
+        }
         return result;
     }
 }
