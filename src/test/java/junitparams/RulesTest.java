2595366170ad6d0c9c7261021f1321564ca60a3b
lipinski pawel <lipinski.pawel@gmail.com>
2011-06-13 17:39:42 +0200

    handle all standard JUnit rules

---
+++ src/test/java/junitparams/RulesTest.java
@@ -0,0 +1,29 @@
+package junitparams;
+
+import org.junit.*;
+import org.junit.rules.*;
+import org.junit.runner.*;
+
+@RunWith(JUnitParamsRunner.class)
+public class RulesTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    @Rule
+    public ErrorCollector errors = new ErrorCollector();
+    @Rule
+    public TestName testName = new TestName();
+    @Rule
+    public TestWatchman testWatchman = new TestWatchman();
+    @Rule
+    public Timeout timeout = new Timeout(0);
+    @Rule
+    public Verifier verifier = new Verifier();
+
+    @Test
+    @Parameters("")
+    public void shouldHandleRulesProperly() throws Exception {
+
+    }
+}
