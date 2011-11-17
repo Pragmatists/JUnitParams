b9a898a3460f634a07e02672a815fb22dc6fe2be
lipinski.pawel <plipinski@users.noreply.github.com>
2011-11-17 11:29:41 +0100

    Fixed issue with wrong class name in exception when faulty params provider class given

---
+++ src/test/java/junitparams/ParametersProvidersTest.java
@@ -0,0 +1,46 @@
+package junitparams;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.internal.matchers.StringContains;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.model.FrameworkMethod;
+
+import java.lang.reflect.Method;
+
+public class ParametersProvidersTest {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void shouldPutProviderClassNameInExceptionMessageForProviderWithNoValidMethods() {
+        //given
+        ParameterisedTestMethodRunner runner = new ParameterisedTestMethodRunner(getTestMethodWithInvalidProvider());
+
+        //when
+        exception.expect(RuntimeException.class);
+        exception.expectMessage(new StringContains(ProviderClassWithNoValidMethods.class.getName()));
+        runner.paramsFromAnnotation();
+
+        //then
+        //expected exception
+    }
+
+    private TestMethod getTestMethodWithInvalidProvider() {
+        Method testMethod = TestClassWithProviderClassWithNoValidMethods.class.getMethods()[0];
+        return new TestMethod(new FrameworkMethod(testMethod));
+    }
+
+    @RunWith(JUnitParamsRunner.class)
+    static class TestClassWithProviderClassWithNoValidMethods {
+        @Test
+        @Parameters(source = ProviderClassWithNoValidMethods.class)
+        public void shouldDoNothingItsJustToConnectTestClassWithProvider() {
+        }
+    }
+
+    static class ProviderClassWithNoValidMethods {
+    }
+}
