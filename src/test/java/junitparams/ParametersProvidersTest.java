69a5b7f48bcb581f2bf9d8615f1b5c789526eaca
lipinski pawel <plipinski@users.noreply.github.com>
2011-12-17 17:18:32 +0200

    method= handles multiple methods now tests are divided into ways params are handled

--- src/test/java/junitparams/ParametersProvidersTest.java
+++ src/test/java/junitparams/ParametersProvidersTest.java
@@ -1,5 +1,8 @@
 package junitparams;
 
+import static junitparams.JUnitParamsRunner.*;
+import static org.junit.Assert.*;
+
 import java.lang.reflect.*;
 
 import org.junit.*;
@@ -12,6 +15,17 @@ public class ParametersProvidersTest {
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    @Parameters(source = SingleParamSetProvider.class)
+    public void oneParamSetFromClass(String a, String b) {
+    }
+
+    static class SingleParamSetProvider {
+        public static Object[] provideOneParamSetSameTypes() {
+            return $($("a", "b"));
+        }
+    }
 
     @Test
     public void shouldPutProviderClassNameInExceptionMessageForProviderWithNoValidMethods() {
@@ -37,4 +51,49 @@ public class ParametersProvidersTest {
 
     static class ProviderClassWithNoValidMethods {
     }
+
+    @Test
+    @Parameters(source = OneIntegerProvider.class)
+    public void providedPrimitiveParams(int integer) {
+        assertTrue(integer < 4);
+    }
+
+    public static class OneIntegerProvider {
+        public static Object[] provideTwoNumbers() {
+            return $($(1), $(2));
+        }
+
+        public static Object[] provideOneNumber() {
+            return new Object[] { $(3) };
+        }
+    }
+
+    @Test
+    @Parameters(source = DomainObjectProvider.class)
+    public void providedDomainParams(DomainClass object1, DomainClass object2) {
+        assertEquals("dupa1", object1.toString());
+        assertEquals("dupa2", object2.toString());
+    }
+
+    public static class DomainObjectProvider {
+        public static Object[] provideDomainObject() {
+            return new Object[] { new Object[] {
+                    new DomainClass("dupa1"),
+                    new DomainClass("dupa2") } };
+        }
+    }
+
+    public static class DomainClass {
+        private final String name;
+
+        public DomainClass(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
 }
