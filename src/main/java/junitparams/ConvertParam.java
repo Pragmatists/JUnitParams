4349ad221afd071f69a1c2b09ceaf379501560a8
lipinski pawel <lipinski.pawel@gmail.com>
2012-12-15 01:02:12 +0100

    Parameter converters handling added

---
+++ src/main/java/junitparams/ConvertParam.java
@@ -0,0 +1,18 @@
+package junitparams;
+
+import java.lang.annotation.*;
+
+/**
+ * 
+ * Defines a converter which should be used to convert a parameter to expected
+ * type.
+ * 
+ * @author Pawel Lipinski
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConvertParam {
+
+    Class<? extends ParamConverter<?>> value();
+
+}
---
+++ src/test/java/junitparams/paramconverters/ParamsConverterTest.java
@@ -0,0 +1,39 @@
+package junitparams.paramconverters;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import junitparams.*;
+
+import org.junit.*;
+import org.junit.runner.*;
+
+@RunWith(JUnitParamsRunner.class)
+public class ParamsConverterTest {
+
+    @Test
+    @Parameters({ "01.12.2012" })
+    public void convertSingleParam(@ConvertParam(DateParamConverter.class) Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        assertEquals(2012, calendar.get(Calendar.YEAR));
+        assertEquals(11, calendar.get(Calendar.MONTH));
+        assertEquals(1, calendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+    @Test
+    @Parameters({ "01.12.2012,A" })
+    public void convertMultipleParams(@ConvertParam(DateParamConverter.class) Date date,
+        @ConvertParam(LetterToNumberConverter.class) int num) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        assertEquals(2012, calendar.get(Calendar.YEAR));
+        assertEquals(11, calendar.get(Calendar.MONTH));
+        assertEquals(1, calendar.get(Calendar.DAY_OF_MONTH));
+
+        assertEquals(1, num);
+    }
+}
