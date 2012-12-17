e5e9a0f415534ec3caf317d5763d904d3ca851dd
lipinski pawel <plipinski@users.noreply.github.com>
2012-12-17 10:04:23 +0100

    moved test converter classes to the test class

--- src/test/java/junitparams/paramconverters/ParamsConverterTest.java
+++ src/test/java/junitparams/paramconverters/ParamsConverterTest.java
@@ -2,6 +2,7 @@ package junitparams.paramconverters;
 
 import static org.junit.Assert.*;
 
+import java.text.*;
 import java.util.*;
 
 import junitparams.*;
@@ -40,4 +41,22 @@ public class ParamsConverterTest {
 
         assertEquals(1, num);
     }
+
+    public static class LetterToNumberConverter implements ParamConverter<Integer> {
+        public Integer convert(Object param, String options) {
+            return param.toString().charAt(0) - 64;
+        }
+    }
+
+    public static class StringToDateConverter implements ParamConverter<Date> {
+        public Date convert(Object param, String options) {
+            try {
+                return new SimpleDateFormat(options).parse(param.toString());
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
 }
