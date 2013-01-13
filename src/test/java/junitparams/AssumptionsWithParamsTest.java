bd5fd5717ddda215ececdef4fbe98176913e0fd6
lipinski.pawel <lipinski.pawel@localhost>
2013-01-13 18:53:31 +0100

    ignores update

--- src/test/java/junitparams/AssumptionsWithParamsTest.java
+++ src/test/java/junitparams/AssumptionsWithParamsTest.java
@@ -1,6 +1,7 @@
 package junitparams;
 
 import static org.junit.Assert.*;
+import static org.junit.Assume.*;
 
 import org.junit.*;
 import org.junit.runner.*;
@@ -11,7 +12,7 @@ public class AssumptionsWithParamsTest {
     @Test
     @Parameters({ "true", "false" })
     public void assumeOnceWorksAndOnceIgnores(boolean value) {
-        Assume.assumeTrue(value);
+        assumeTrue(value);
         assertTrue(value);
     }
 }
