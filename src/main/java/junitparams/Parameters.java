826e126e414b67e73f3dc585b1706ae7a4c11e4b
Michal Lipski <kiwaczki@gmail.com>
2015-02-05 15:20:19 +0100

    #30 Unable to pass newlines as a parameter

--- src/main/java/junitparams/Parameters.java
+++ src/main/java/junitparams/Parameters.java
@@ -16,6 +16,8 @@ public @interface Parameters {
      * Parameter values defined as a String array. Each element in the array is
      * a full parameter set, comma-separated or pipe-separated ('|').
      * The values must match the method parameters in order and type.
+     * Whitespace characters are trimmed (use source class or method if You need to provide such parameters)
+     *
      * Example: <code>@Parameters({
      *                    "1, joe, 26.4, true", 
      *                    "2, angie, 37.2, false"})</code>
