956f8428ebde5379676a93d9a8cf29de5149fa2d
Michael Bareja <michal.bareja@pragmatists.pl>
2016-03-25 13:13:25 +0100

    BigDecimalConverion does not need PropertyEditor anymore

--- src/test/java/junitparams/ParametersConvertedWithPropertyEditorTest.java
+++ src/test/java/junitparams/ParametersConvertedWithPropertyEditorTest.java
@@ -1,38 +1,44 @@
 package junitparams;
 
-import static org.assertj.core.api.Assertions.*;
-
 import java.beans.PropertyEditorManager;
 import java.beans.PropertyEditorSupport;
-import java.math.BigDecimal;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.assertj.core.api.Assertions.*;
+
 @RunWith(JUnitParamsRunner.class)
 public class ParametersConvertedWithPropertyEditorTest {
 
-	@BeforeClass
-	public static void registerEditors() {
-		PropertyEditorManager.registerEditor(BigDecimal.class, BigDecimalPropertyEditor.class);
-	}
+    @BeforeClass
+    public static void registerEditors() {
+        PropertyEditorManager.registerEditor(StringWrapper.class, StringWrapperPropertyEditor.class);
+    }
 
-	@Test
-	@Parameters({
-			"123456789.9876543212 , 123456789.9876543212",
-			"-99887766.5544332211 , -99887766.5544332211000",
-			"0.000000000000000001 , 0.000000000000000001",
-			"0, 0"
-	})
-	public void convertsToBigDecimal(BigDecimal num, String numAsText) {
-		assertThat(num).isEqualByComparingTo(numAsText);
-	}
+    @Test
+    @Parameters({"wrapped , wrapped"})
+    public void convertsToCustomType(StringWrapper wrapper, String text) {
+        assertThat(wrapper.getText()).isEqualTo(text);
+    }
 
-	public static class BigDecimalPropertyEditor extends PropertyEditorSupport {
+    public static class StringWrapperPropertyEditor extends PropertyEditorSupport {
         @Override
         public void setAsText(String text) throws IllegalArgumentException {
-			setValue(new BigDecimal(text));
+            setValue(new StringWrapper(text));
+        }
+    }
+
+    private static class StringWrapper {
+        private String text;
+
+        StringWrapper(String text) {
+            this.text = text;
+        }
+
+        String getText() {
+            return text;
         }
     }
 }
