925ef3a0d3117a52ce60a393953efffc420f4d0b
邱占波 <bohr.qiu@gmail.com>
2017-03-25 12:12:43 +0800

    enhance ParametersProvider  that can get  FrameworkMethod

--- src/main/java/junitparams/internal/parameters/ParametersFromCustomProvider.java
+++ src/main/java/junitparams/internal/parameters/ParametersFromCustomProvider.java
@@ -6,11 +6,14 @@ import junitparams.custom.ParametersProvider;
 import junitparams.internal.annotation.CustomParametersDescriptor;
 import junitparams.internal.annotation.FrameworkMethodAnnotations;
 
+import java.lang.reflect.Constructor;
+
 class ParametersFromCustomProvider implements ParametrizationStrategy {
 
     private final FrameworkMethodAnnotations frameworkMethodAnnotations;
-
+    private final FrameworkMethod frameworkMethod;
     ParametersFromCustomProvider(FrameworkMethod frameworkMethod) {
+        this.frameworkMethod=frameworkMethod;
         frameworkMethodAnnotations = new FrameworkMethodAnnotations(frameworkMethod);
     }
 
@@ -28,11 +31,28 @@ class ParametersFromCustomProvider implements ParametrizationStrategy {
     }
 
     private ParametersProvider instantiate(Class<? extends ParametersProvider> providerClass) {
-        try {
-            return providerClass.newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException("Your Provider class must have a public no-arg constructor!", e);
+        Constructor<?>[] constructors = providerClass.getConstructors();
+        for (Constructor<?> constructor : constructors) {
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+            if(parameterTypes.length==1){
+                if(parameterTypes[0] == FrameworkMethod.class){
+                    try {
+                        return (ParametersProvider) constructor.newInstance(frameworkMethod);
+                    } catch (Exception e) {
+                        throw new RuntimeException("init error", e);
+                    }
+                }else{
+                    throw new RuntimeException("init error,1-arg constructor parameter type must be FrameworkMethod!");
+                }
+            }else {
+                try {
+                    return (ParametersProvider) constructor.newInstance();
+                } catch (Exception e) {
+                    throw new RuntimeException("Your Provider class must have a public no-arg constructor!", e);
+                }
+            }
         }
+        throw new RuntimeException("Your Provider class must have a public no-arg constructor!");
     }
 
 }
